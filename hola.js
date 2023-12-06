9beec53101a8dc9cb0d8c327dd72bae5fb1fc3c8
EriEstrada2 <eriika961@gmail.com>
2023-12-05 20:50:12 -0500

    Actualizacion de los scripts

--- hola.js
+++ hola.js
@@ -1,1 +1,12 @@
-console.log("Mejor este mensaje no va")
+function redirigir() {
+    var selectElement = document.getElementById('miOpcion');
+    var selectedValue = selectElement.value;
+
+    // Verifica el valor y redirige según la opción seleccionada
+    if (selectedValue === 'opcion1') {
+        // No hagas nada o puedes ejecutar alguna lógica específica
+    } else if (selectedValue === 'opcion2') {
+        window.location.href = 'index.html'; // Cambia esto al nombre de tu página docente
+    }
+    // Puedes agregar más condiciones según sea necesario
+}
